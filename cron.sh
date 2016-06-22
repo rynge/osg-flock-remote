a96117e77e4de7f231ee9bee47741be3425fb19f
Mats Rynge <mats@rynge.net>
2016-06-22 11:09:17 -0700

    Fix find expressions

--- cron.sh
+++ cron.sh
@@ -7,15 +7,15 @@ cd $TOP_DIR
 echo "Running in $PWD"
 
 # only update from git every so often
-find . -max-level 1 -name .git-update.ts -mmin +720 -exec rm -f {} \;
-if [ ! -e .gratia.ts ]; then
+find . -maxdepth 1 -name .git-update.ts -mmin +720 -exec rm -f {} \;
+if [ ! -e .git-update.ts ]; then
     git fetch --all
     git reset --hard origin/master
     touch .git-update.ts
 fi
 
 # lock
-find . -max-level 1 -name .cron.lock -mmin +60 -exec rm -f {} \;
+find . -maxdepth 1 -name .cron.lock -mmin +60 -exec rm -f {} \;
 if [ -e .cron.lock ]; then
     exit 0
 fi
